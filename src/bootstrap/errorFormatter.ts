cff119d465c83ee4d3fd0b44a05fff935a0cb7d1
Martin Štěpánek <mstepanek015@gmail.com>
2020-10-08 17:10:46 +0200

    refactor: change user message to enum

--- src/bootstrap/errorFormatter.ts
+++ src/bootstrap/errorFormatter.ts
@@ -3,7 +3,7 @@ import { GraphQLError } from 'graphql';
 
 export const errorFormatter = (error: GraphQLError): GraphQLError => {
     if (error.originalError?.message.startsWith('Access denied')) {
-        return new AuthenticationError('User not logged in');
+        return new AuthenticationError('INVALID_TOKEN');
     }
     return error;
 };
