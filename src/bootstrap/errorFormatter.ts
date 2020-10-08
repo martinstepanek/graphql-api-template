81a843cd58e782f1f0322759a0ad2bc9cdd9d434
Martin Štěpánek <mstepanek015@gmail.com>
2020-10-08 16:00:42 +0200

    fix: errorFormatter

--- src/bootstrap/errorFormatter.ts
+++ src/bootstrap/errorFormatter.ts
@@ -2,7 +2,7 @@ import { AuthenticationError } from 'apollo-server-express';
 import { GraphQLError } from 'graphql';
 
 export const errorFormatter = (error: GraphQLError): GraphQLError => {
-    if (error.originalError.message.startsWith('Access denied')) {
+    if (error.originalError?.message.startsWith('Access denied')) {
         return new AuthenticationError('User not logged in');
     }
     return error;
